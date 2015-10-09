5cc807a2a19b67155dc9f0e3ea2ceeed0c181982
Venj <ersaclarke@gmail.com>
2015-10-09 13:52:26 +0800

    No longer need to dismiss modal while showing passcode view.

--- Swift Photos/AppDelegate.swift
+++ Swift Photos/AppDelegate.swift
@@ -54,7 +54,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MMAppSwitcherDataSource {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        window?.rootViewController?.dismissViewControllerAnimated(false, completion: nil)
         passcodeLockPresenter.presentPasscodeLock()
     }
 
